45d6a3a2fb166b5e02ecfe5ecb422a1382574ec2
Mario Espada <mario.espada77@gmail.com>
2020-05-19 03:34:40 +0200

    ADD Follow model

---
+++ models/Follow.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+const FollowSchema = new Schema(
+  {
+    followed: {
+      type: Schema.Types.ObjectId,
+      ref: "User"
+    },
+    follower: {
+      type: Schema.Types.ObjectId,
+      ref: "User"
+    }
+  },
+  {
+    timestamps: {
+      createdAt: "created_at",
+      updatedAt: "updated_at"
+    }
+  }
+);
+const Follow = mongoose.model("Follow", FollowSchema);
