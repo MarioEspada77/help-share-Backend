fc5b33fece775b9f401d1f36badce7cd0eacad09
Mario Espada <mario.espada77@gmail.com>
2020-05-19 03:28:47 +0200

    ADD Post model

---
+++ models/Post.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+const PostSchema = new Schema(
+  {
+    username: {
+      type: Schema.Types.ObjectId,
+      ref: "User"
+    },
+    text: { type: String },
+    likes: [{
+      type: Schema.Types.ObjectId,
+      ref: 'Likes'
+    }],
+    share: {
+      type: Schema.Types.ObjectId,
+      ref: "User"
+    },
+    commented_to: {
+      type: Schema.Types.ObjectId,
+      ref: "Post"
+    }
+
+  },
+  {
+    timestamps: {
+      createdAt: "created_at",
+      updatedAt: "updated_at"
+    }
+  }
+);
+const Post = mongoose.model("Post", PostSchema);
+
+module.exports = Post;
