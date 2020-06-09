679a284954eccbc5c0b97abc6b936636e74946e8
Mario Espada <mario.espada77@gmail.com>
2020-06-09 22:57:52 +0200

    ADD Routes Posts and Follows

---
+++ routes/follow.js
@@ -0,0 +1,62 @@
+var express = require("express");
+var router = express.Router();
+
+const Post = require("../models/Post");
+const User = require("../models/User");
+const Follow = require("../models/Follow");
+
+router.get("/:followed/:follower/follow", async (req, res, next) => {
+  const { followed, follower } = req.params;
+  try {
+    const userfollowed = await User.find({ username: followed });
+    const userWhoFollow = await User.find({ username: follower });
+    const follow = await Follow.create({
+      followed: userfollowed[0]._id,
+      follower: userWhoFollow[0]._id
+    });
+    res.json(follow);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/:followed/getUsers", async (req, res, next) => {
+  const { followed } = req.params;
+
+  try {
+    const user = await User.find({ username: followed });
+    const userfollowed = await Follow.find({ followed: user[0]._id }).populate(
+      "follower"
+    );
+    res.json(userfollowed);
+  } catch (error) {
+    next(error);
+  }
+});
+router.get("/:follower/getFollowing", async (req, res, next) => {
+  const { follower } = req.params;
+
+  try {
+    const user = await User.find({ username: follower });
+    const userfollowing = await Follow.find({ follower: user[0]._id }).populate(
+      "followed"
+    );
+    res.json(userfollowing);
+  } catch (error) {
+    next(error);
+  }
+});
+router.get("/:followId/unfollow", async (req, res, next) => {
+  const { followId } = req.params;
+
+  try {
+    const unfollow = await Follow.findByIdAndDelete(followId);
+    console.log(unfollow);
+    res.json(unfollow);
+  } catch (error) {
+    next(error);
+  }
+});
+
+
+module.exports = router;
---
+++ routes/post.js
@@ -0,0 +1,85 @@
+const express = require("express");
+const router = express.Router();
+
+const Post = require("../models/Post");
+const User = require("../models/User");
+const Follow = require("../models/Follow");
+const mongoose = require("mongoose");
+
+router.post("/:username/new", async (req, res, next) => {
+  const { username } = req.params;
+  const { text, university, post_id } = req.body;
+  try {
+    const user = await User.find({ username });
+    console.log(user[0].username);
+    const post = await Post.create({
+      username: user[0]._id,
+      text,
+      formUni: university,
+      commented_to: post_id
+    });
+    res.json({
+      status: 200,
+      post
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+router.get("/detail/:postId", async (req, res, next) => {
+  const { postId } = req.params;
+  try {
+    const postDetail = await Post.findById(postId).populate("username");
+    const postComments = await Post.find({commented_to: postId }).populate("username").sort("-created_at");;
+    res.json({postDetail, postComments});
+  } catch (error) {
+    console.log(error);
+  }
+});
+router.get("/all", async (req, res, next) => {
+  const username = req.session.currentUser;
+  console.log(username);
+  try {
+    const followsId = await Follow.find(
+      { follower: username._id },
+      "followed -_id"
+    );
+    const arr = followsId.map(elem => elem.followed);
+    const post = await Post.find({
+      $or: [{ username: arr }, { formUni: arr }, { username: username._id }]
+    })
+      .populate("username")
+      .populate("formUni")
+      .sort("-created_at");
+    res.json(post);
+  } catch (error) {
+    next(error);
+  }
+});
+router.get("/:postId/:username/like", async (req, res, next) => {
+  const { postId, username } = req.params;
+  try {
+    const user = await User.find({ username });
+    const like = await Post.findByIdAndUpdate(postId, {
+      $push: { likes: user[0]._id }
+    }).populate("username");
+    console.log(like);
+    res.json(like);
+  } catch (error) {
+    next(error);
+  }
+});
+router.get("/:postId/:username/unlike", async (req, res, next) => {
+  const { postId, username } = req.params;
+  try {
+    const user = await User.find({ username });
+    const like = await Post.findByIdAndUpdate(postId, {
+      $pull: { likes: user[0]._id }
+    }).populate("username");
+    res.json(like);
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
